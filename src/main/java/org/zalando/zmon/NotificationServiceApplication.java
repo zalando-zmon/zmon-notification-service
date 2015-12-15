e280a673b83dc404579fef3e4b3090a5ae696582
Hachmann <b.hachmann@gmail.com>
2015-12-15 17:42:57 +0100

    store/remove devices and alerts in redis

--- src/main/java/org/zalando/zmon/NotificationServiceApplication.java
+++ src/main/java/org/zalando/zmon/NotificationServiceApplication.java
@@ -9,17 +9,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @SpringBootApplication
 public class NotificationServiceApplication {
+    private static final String NOTIFICATION_DEVICES = "zmon:push:%s";
+    private static final String NOTIFICATION_ALERTS = "zmon:alert:%d";
+
+    @Value("${oauth.tokeninfo.url:}")
+    String tokenInfoUrl;
 
     @Bean
     JedisPool getRedisPool(NotificationServiceConfig config) throws URISyntaxException {
@@ -27,15 +32,14 @@ public class NotificationServiceApplication {
         return new JedisPool(poolConfig, new URI(config.getRedisUri()));
     }
 
-    @Value("${oauth.tokeninfo.url:}")
-    String tokenInfoUrl;
-
     @Bean
     TokenInfoService getTokenInfoService() {
         return new TokenInfoService(tokenInfoUrl);
     }
 
-    public static class DeviceRequestBody{
+    // request payloads
+
+    public static class DeviceRequestBody {
         public String registration_token;
     }
 
@@ -50,30 +54,98 @@ public class NotificationServiceApplication {
     }
 
     @Autowired
-    JedisPool pool;
-
-    @Value("${oauth.enabled:1}")
-    private boolean oauthEnabled;
+    JedisPool jedisPool;
 
     @Autowired
     TokenInfoService tokenInfoService;
 
-    @RequestMapping(value="/api/v1/device", method = RequestMethod.POST)
-    public ResponseEntity<String> registerDeviceToken(@RequestBody SubscriptionRequestBody body, @RequestHeader(value="Authorization", required=false) String oauthHeader) {
-        if(oauthEnabled && !tokenInfoService.isValidHeader(oauthHeader)) {
-            // intellij complains with null
-            return new ResponseEntity<>( "", HttpStatus.UNAUTHORIZED);
+    // registering
+
+    @RequestMapping(value = "/api/v1/device", method = RequestMethod.POST)
+    public ResponseEntity<String> registerDevice(@RequestBody DeviceRequestBody body, @RequestHeader(value = "Authorization", required = false) String oauthHeader) {
+        Optional<String> uid = tokenInfoService.lookupUid(oauthHeader);
+        if (uid.isPresent()) {
+            Jedis jedis = jedisPool.getResource();
+            try {
+                String deviceToken = body.registration_token;
+                String redisKey = String.format(NOTIFICATION_DEVICES, uid.get());
+                jedis.sadd(redisKey, deviceToken);     // this redis set contains all the devices registered for a specific oauth uid
+            } finally {
+                jedisPool.returnResource(jedis);
+            }
+
+            return new ResponseEntity<>("", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("", HttpStatus.UNAUTHORIZED);
         }
-        return new ResponseEntity<>("", HttpStatus.OK);
     }
 
-    @RequestMapping(value="/api/v1/subscription", method = RequestMethod.POST)
-    public void registerSubscription(@RequestBody SubscriptionRequestBody body) {
+    @RequestMapping(value = "/api/v1/subscription", method = RequestMethod.POST)
+    public ResponseEntity<String> registerSubscription(@RequestBody SubscriptionRequestBody body, @RequestHeader(value = "Authorization", required = false) String oauthHeader) {
+        Optional<String> uid = tokenInfoService.lookupUid(oauthHeader);
+        if (uid.isPresent()) {
+            Jedis jedis = jedisPool.getResource();
+            try {
+                String redisKey = String.format(NOTIFICATION_ALERTS, body.alert_id);
+                jedis.sadd(redisKey, uid.get());     // this redis set contains all the users registered for a specific alert id
+            } finally {
+                jedisPool.returnResource(jedis);
+            }
+
+            return new ResponseEntity<>("", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("", HttpStatus.UNAUTHORIZED);
+        }
     }
 
-    @RequestMapping(value="/api/v1/publish", method = RequestMethod.POST)
+
+    // publishing new alerts
+
+    @RequestMapping(value = "/api/v1/publish", method = RequestMethod.POST)
     public void publishNotification(@RequestBody PublishRequestBody body) {
+
     }
+
+
+    // Unregistering
+
+    @RequestMapping(value = "/api/v1/device", method = RequestMethod.DELETE)
+    public ResponseEntity<String> unregisterDevice(@RequestBody DeviceRequestBody body, @RequestHeader(value = "Authorization", required = false) String oauthHeader) {
+        Optional<String> uid = tokenInfoService.lookupUid(oauthHeader);
+        if (uid.isPresent()) {
+            Jedis jedis = jedisPool.getResource();
+            try {
+                String deviceToken = body.registration_token;
+                String redisKey = String.format("zmon:push:%s", uid.get());
+                jedis.srem(redisKey, deviceToken); // remove device from user
+            } finally {
+                jedisPool.returnResource(jedis);
+            }
+
+            return new ResponseEntity<>("", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @RequestMapping(value = "/api/v1/subscription", method = RequestMethod.DELETE)
+    public ResponseEntity<String> unregisterSubscription(@RequestBody SubscriptionRequestBody body, @RequestHeader(value = "Authorization", required = false) String oauthHeader) {
+        Optional<String> uid = tokenInfoService.lookupUid(oauthHeader);
+        if (uid.isPresent()) {
+            Jedis jedis = jedisPool.getResource();
+            try {
+                String redisKey = String.format("zmon:alert:%d", body.alert_id);
+                jedis.srem(redisKey, uid.get());
+            } finally {
+                jedisPool.returnResource(jedis);
+            }
+
+            return new ResponseEntity<>("", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
--- src/main/java/org/zalando/zmon/TokenInfoService.java
+++ src/main/java/org/zalando/zmon/TokenInfoService.java
@@ -2,81 +2,61 @@ package org.zalando.zmon;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by jmussler on 8/12/15.
  */
 public class TokenInfoService {
-
     private static final Logger LOG = LoggerFactory.getLogger(TokenInfoService.class);
 
     private final String url;
     private final Executor executor;
     private final ObjectMapper mapper = new ObjectMapper();
 
-    private final LoadingCache<String, Boolean> tokenCache;
+    // oauthtoken -> uid
+    private final Cache<String, String> tokenCache = CacheBuilder.newBuilder().expireAfterWrite(600, TimeUnit.SECONDS).build();
 
     public TokenInfoService(String url) {
         this.url = url;
         this.executor = Executor.newInstance();
-
-        tokenCache = CacheBuilder.newBuilder().expireAfterWrite(600, TimeUnit.SECONDS).build(
-                new CacheLoader<String, Boolean>() {
-                    @Override
-                    public Boolean load(String s) throws Exception {
-                        return null;
-                    }
-                });
     }
 
-    public boolean isValidHeader(String header) {
-        if(null == header) return false;
-        if("".equals(header)) return false;
-        return isValidToken(header.replace("Bearer ", ""));
+    public Optional<String> lookupUid(String header) {
+        if (Strings.isNullOrEmpty(header)) {
+            return Optional.empty();
+        }
+
+        return queryCacheOrServer(header.replace("Bearer ", ""));
     }
 
-    public boolean isValidToken(String token) {
-        if(null==token) {
-            return false;
-        }
-
-        if("".equals(token)) {
-            return false;
-        }
-
+    private Optional<String> queryCacheOrServer(String oauthToken) {
         try {
-            Boolean v = tokenCache.get(token);
-            if (v != null && v.booleanValue()== true) {
-                return true;
-            }
+            return Optional.ofNullable(
+                    tokenCache.get(oauthToken, () -> queryOAuthServer(oauthToken))
+            );
+        } catch (Exception e) {
+            LOG.error("Error querying oauth server for token: " + oauthToken, e);
+            return Optional.empty();
         }
-        catch(Exception ex) {
+    }
 
+    private String queryOAuthServer(String token) throws Exception {
+        // throws exception in != 200 status code
+        String body = executor.execute(Request.Get(url + token)).returnContent().asString();
+        JsonNode result = mapper.readTree(body);
+        if (result.has("uid") && result.get("expires_in").asInt() > 0) {
+            return result.get("uid").asText();
         }
-
-        try {
-            // throws exception in != 200 status code
-            String body = executor.execute(Request.Get(url + token)).returnContent().asString();
-            JsonNode result = mapper.readTree(body);
-            if(result.has("uid") && result.get("expires_in").asInt()>0) {
-                tokenCache.put(token, true);
-                return true;
-            }
-
-        } catch (IOException e) {
-            LOG.error("", e);
-        }
-
-        return false;
+        return null;
     }
 }
