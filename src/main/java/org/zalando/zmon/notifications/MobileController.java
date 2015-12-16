c67c579422a5072896e70832d6ead43021f8d63f
Jan-M <janm81@gmail.com>
2015-12-16 17:02:13 +0100

    Proxy all mobile requests now

--- src/main/java/org/zalando/zmon/notifications/MobileController.java
+++ src/main/java/org/zalando/zmon/notifications/MobileController.java
@@ -2,14 +2,17 @@ package org.zalando.zmon.notifications;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.zalando.zmon.notifications.oauth.TokenInfoService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Optional;
 
 /**
@@ -25,35 +28,65 @@ public class MobileController {
     @Autowired
     MobileAPIConfig config;
 
-    public static class AlertHeader {
-        public String name;
-        public String id;
-        public String team;
-    }
-
     @Autowired
     ObjectMapper mapper;
 
     @RequestMapping(path="alert", method= RequestMethod.GET)
-    public ResponseEntity<List<AlertHeader>> getAllAlerts(@RequestHeader(value = "Authorization", required = false) String oauthHeader) {
-        Optional<String> uid = tokenInfoService.lookupUid(oauthHeader);
-        if (!uid.isPresent()) {
-            return new ResponseEntity<>((List)null, HttpStatus.UNAUTHORIZED);
-        }
-
-        List<AlertHeader> alerts = new ArrayList<>();
-        return new ResponseEntity<>(alerts, HttpStatus.OK);
-    }
-
-    @RequestMapping(path="alert/{alert_id}", method=RequestMethod.GET)
-    public ResponseEntity<JsonNode> getAlertDetails(@PathVariable(value="alert_id") int alertId, @RequestHeader(value = "Authorization", required = false) String oauthHeader)
-    {
+    public ResponseEntity<JsonNode> getAllAlerts(@RequestHeader(value = "Authorization", required = false) String oauthHeader) throws URISyntaxException, IOException {
         Optional<String> uid = tokenInfoService.lookupUid(oauthHeader);
         if (!uid.isPresent()) {
             return new ResponseEntity<>((JsonNode)null, HttpStatus.UNAUTHORIZED);
         }
 
-        JsonNode node = mapper.createObjectNode();
+        URI uri = new URIBuilder().setPath(config.dataServiceURL + "/api/v1/mobile/alert").build();
+        final String r = Request.Get(uri).addHeader("Authorization", oauthHeader).useExpectContinue().execute().returnContent().asString();
+
+        JsonNode node = mapper.readTree(r);
+        return new ResponseEntity<>( node, HttpStatus.OK);
+    }
+
+    @RequestMapping(path="alert/{alert_id}", method=RequestMethod.GET)
+    public ResponseEntity<JsonNode> getAlertDetails(@PathVariable(value="alert_id") int alertId, @RequestHeader(value = "Authorization", required = false) String oauthHeader) throws URISyntaxException, IOException {
+        Optional<String> uid = tokenInfoService.lookupUid(oauthHeader);
+        if (!uid.isPresent()) {
+            return new ResponseEntity<>((JsonNode)null, HttpStatus.UNAUTHORIZED);
+        }
+
+        URI uri = new URIBuilder().setPath(config.dataServiceURL + "/api/v1/mobile/alert").addParameter("alert_id", ""+alertId).build();
+        final String r = Request.Get(uri).addHeader("Authorization", oauthHeader).useExpectContinue().execute().returnContent().asString();
+
+        JsonNode node = mapper.readTree(r);
+
+        return new ResponseEntity<>( node, HttpStatus.OK);
+    }
+
+    @RequestMapping(path="all-teams", method=RequestMethod.GET)
+    public ResponseEntity<JsonNode> getAllTeams(@RequestHeader(value = "Authorization", required = false) String oauthHeader) throws URISyntaxException, IOException {
+        Optional<String> uid = tokenInfoService.lookupUid(oauthHeader);
+        if (!uid.isPresent()) {
+            return new ResponseEntity<>((JsonNode)null, HttpStatus.UNAUTHORIZED);
+        }
+
+        URI uri = new URIBuilder().setPath(config.dataServiceURL + "/api/v1/mobile/all-teams").build();
+        final String r = Request.Get(uri).addHeader("Authorization", oauthHeader).useExpectContinue().execute().returnContent().asString();
+
+        JsonNode node = mapper.readTree(r);
+
+        return new ResponseEntity<>( node, HttpStatus.OK);
+    }
+
+    @RequestMapping(path="status", method=RequestMethod.GET)
+    public ResponseEntity<JsonNode> getStatus(@RequestHeader(value = "Authorization", required = false) String oauthHeader) throws URISyntaxException, IOException {
+        Optional<String> uid = tokenInfoService.lookupUid(oauthHeader);
+        if (!uid.isPresent()) {
+            return new ResponseEntity<>((JsonNode)null, HttpStatus.UNAUTHORIZED);
+        }
+
+        URI uri = new URIBuilder().setPath(config.dataServiceURL + "/api/v1/mobile/status").build();
+        final String r = Request.Get(uri).addHeader("Authorization", oauthHeader).useExpectContinue().execute().returnContent().asString();
+
+        JsonNode node = mapper.readTree(r);
+
         return new ResponseEntity<>( node, HttpStatus.OK);
     }
 
