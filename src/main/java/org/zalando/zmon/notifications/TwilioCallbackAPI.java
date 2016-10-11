d3a0cf014c770f64e3cfc1d145a8d2ce4c6a9b78
Jan M <janm81@gmail.com>
2016-10-11 16:03:09 +0200

    Pushing response code

--- src/main/java/org/zalando/zmon/notifications/TwilioCallbackAPI.java
+++ src/main/java/org/zalando/zmon/notifications/TwilioCallbackAPI.java
@@ -1,5 +1,7 @@
 package org.zalando.zmon.notifications;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +14,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/twilio")
 public class TwilioCallbackAPI {
 
+    private final Logger log = LoggerFactory.getLogger(TwilioCallbackAPI.class);
+
     @RequestMapping(method = RequestMethod.POST, produces = "application/xml")
     public String getDummyResult(@RequestParam(name = "id") String id) {
+        log.info("Receiving request for id={}", id);
         return "<Response>\n" +
                 "        <Say voice=\"woman\">ZMON Alert Active - PLANB ELB DOWN</Say>\n" +
-                "        <Gather timeout=\"10\" finishOnKey=\"*\">\n" +
+                "        <Gather action=\"/api/v1/twilio/response\" method=\"POST\" timeout=\"10\" finishOnKey=\"*\">\n" +
                 "          <Say voice=\"woman\">Please enter 1 for ACK and 6 for Resolve.</Say>\n" +
                 "        </Gather>\n" +
                 "</Response>";
+    }
+
+    @RequestMapping(path="/response", method = RequestMethod.POST, produces = "application/xml")
+    public String ackNotification(@RequestParam(name = "id") String id, @RequestParam(name="Digits") String digits) {
+        log.info("Receiving request for id={} digits={}", id, digits);
+        if("1".equals(digits)) {
+            return "<Response><Say>Acknowledged</Say></Response>";
+        }
+        else if("6".equals(digits)) {
+            return "<Response><Say>Resolved</Say></Response>";
+        }
+        else {
+            return "<Response><Say>ZMON Response Error</Say></Response>";
+        }
     }
 }
