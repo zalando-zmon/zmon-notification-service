aaeb62a25bdbf3c2d59bea1bc0af2521f2a4a5a2
Jan M <janm81@gmail.com>
2016-11-08 15:35:31 +0100

    Fixing dry run setter

--- src/main/java/org/zalando/zmon/notifications/NotificationServiceConfig.java
+++ src/main/java/org/zalando/zmon/notifications/NotificationServiceConfig.java
@@ -69,7 +69,7 @@ public class NotificationServiceConfig {
     }
 
     public void setDryRun(boolean dryRun) {
-        dryRun = dryRun;
+        this.dryRun = dryRun;
     }
 
     public boolean isEventlogEnabled() {
