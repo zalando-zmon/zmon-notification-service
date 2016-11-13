d15f8700fedc221bab8c3d30dda507328078a123
Jan-M <janm81@gmail.com>
2016-11-13 15:41:54 +0100

    Improve test script

--- test-gcm.sh
+++ test-gcm.sh
@@ -1,1 +1,7 @@
-curl -H "Content-Type: application/json" -H "Authorization: key=" -d@test.json -v -X POST https://gcm-http.googleapis.com/gcm/send
+#!/bin/bash
+
+curl -H "Content-Type: application/json" \
+     -H "Authorization: key=$KEY" \
+     -H "Content-type: application/json" \
+     -d "{\"to\":\"$DEVICEID\", \"notification\": {\"title\":\"CPU above 80%\", \"icon\":\"clean.png\", \"click_action\":\"https://localhost:8444/#/alert-details/view/1\"}}" \
+     -v -X POST https://gcm-http.googleapis.com/gcm/send
