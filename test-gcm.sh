7feb30bf28b40ae4114fdbb7454eea8cbce4c312
Jan-M <janm81@gmail.com>
2016-11-13 16:06:24 +0100

    Body spec

--- test-gcm.sh
+++ test-gcm.sh
@@ -3,5 +3,5 @@
 curl -H "Content-Type: application/json" \
      -H "Authorization: key=$KEY" \
      -H "Content-type: application/json" \
-     -d "{\"to\":\"$DEVICEID\", \"notification\": {\"title\":\"CPU above 80%\", \"icon\":\"clean.png\", \"click_action\":\"https://localhost:8444/#/alert-details/view/1\"}}" \
+     -d "{\"to\":\"$DEVICEID\", \"notification\": {\"title\":\"CPU above 80%\", \"body\": \"controller-xvfes[west]\" \"icon\":\"clean.png\", \"click_action\":\"https://localhost:8444/#/alert-details/view/1\"}}" \
      -v -X POST https://gcm-http.googleapis.com/gcm/send
