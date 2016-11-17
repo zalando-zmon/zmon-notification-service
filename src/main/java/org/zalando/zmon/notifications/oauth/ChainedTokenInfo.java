f20bb7b208d53a65852eb6a2395a692de46bfdc0
Jan M <janm81@gmail.com>
2016-11-17 18:29:56 +0100

    Logging token info services used

--- src/main/java/org/zalando/zmon/notifications/oauth/ChainedTokenInfo.java
+++ src/main/java/org/zalando/zmon/notifications/oauth/ChainedTokenInfo.java
@@ -1,5 +1,8 @@
 package org.zalando.zmon.notifications.oauth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Optional;
 
 /**
@@ -7,10 +10,15 @@ import java.util.Optional;
  */
 public class ChainedTokenInfo implements TokenInfoService {
 
+    private final Logger log = LoggerFactory.getLogger(ChainedTokenInfo.class);
+
     TokenInfoService[] services;
 
     public ChainedTokenInfo(TokenInfoService... service) {
             services = service;
+            for(TokenInfoService s : services) {
+                log.info("TokenInfo entry: {}", s.getClass());
+            }
     }
 
     @Override
